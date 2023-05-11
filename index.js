569f7587eb02957c16776688b000b069f514cd71
Johnny Proano <jproano22@yahoo.com>
2023-05-11 13:43:17 -0400

    added weather style with flex and size

--- index.js
+++ index.js
@@ -44,10 +44,12 @@ navigator.geolocation.getCurrentPosition((position) => {
       return res.json();
     })
     .then((data) => {
-      // console.log(data)
+      console.log(data);
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
-              <img src=${iconUrl} />
+      <img src=${iconUrl} />
+      <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+      <p class="weather-city">${data.name}</p>
           `;
     })
     .catch((err) => console.error(err));
