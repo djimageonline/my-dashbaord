814b21e65e8347c49804eccb716e7ba96c06f898
Johnny Proano <jproano22@yahoo.com>
2023-05-11 11:19:50 -0400

    added dogecoin api fetch, .then and catch.  Added div to display crypto and added style

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+  })
+  .catch((err) => console.error(err));
